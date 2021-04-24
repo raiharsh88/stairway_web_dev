32d4debc9cfbefc09181081b34e8cba53821af36
Harsh rai <raiharsh88@gmail.com>
2021-04-24 10:40:45 +0530

    regular backup

--- front_end/pages/login.jsx
+++ front_end/pages/login.jsx
@@ -15,12 +15,9 @@ const HeadComp = function (props) {
 
             {/* <script src="https://kit.fontawesome.com/50f0e7155c.js" crossorigin="anonymous"></script> */}
 
-            <link
-                rel="stylesheet"
-                href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-                integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-                crossOrigin="anonymous"
-            />        </Head>
+           
+
+             </Head>
     )
 }
 
